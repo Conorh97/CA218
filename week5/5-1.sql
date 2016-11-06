ee6be6b4c34d0ee7ba4a073a35e2405d6ac8939d
Conor Hanlon <conor.hanlon5@mail.dcu.ie>
2016-11-06 12:52:41 +0000

    Add files via upload

---
+++ week5/5-1.sql
@@ -0,0 +1,6 @@
+SELECT f.film_id, f.title 
+FROM film f, inventory i, rental r
+WHERE f.film_id = i.film_id and (SELECT COUNT(r.inventory_id)
+FROM rental r, customer c
+WHERE r.inventory_id = i.inventory_id and r.customer_id = c.customer_id and c.first_name LIKE "D%") > 1                                                                    
+GROUP BY i.film_id;
