1f9e2d58d04189f9491bfbf8f27a9ff38958ac11
Conor Hanlon <conor.hanlon5@mail.dcu.ie>
2016-11-10 19:08:48 +0000

    Delete 5-3.sql

--- week5/5-3.sql
+++
@@ -1,7 +0,0 @@
-SELECT f.title, c.name, (SELECT COUNT(r.rental_id)
-FROM rental r 
-WHERE r.inventory_id = i.inventory_id and i.store_id = 1) as borrowCount                        
-FROM film f, film_category fc, category c, inventory i
-WHERE f.film_id = i.film_id and f.film_id = fc.film_id and fc.category_id = c.category_id 
-GROUP BY f.film_id
-ORDER BY f.title;
