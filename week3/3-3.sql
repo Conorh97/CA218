2fc7997edd68f01da8f2098983ad0f6291d0d8aa
Conor Hanlon <conor.hanlon5@mail.dcu.ie>
2016-11-14 20:08:48 +0000

    Update 3-3.sql

--- week3/3-3.sql
+++ week3/3-3.sql
@@ -1,3 +1,7 @@
+-- For each country, print out the name of the country and the number of
+-- districts that country has. Order the results by the
+-- number of districts from highest to lowest.
+
 SELECT co.Name, COUNT(DISTINCT(ci.District)) 
 FROM Country co, City ci 
 WHERE co.Code = ci.CountryCode 
