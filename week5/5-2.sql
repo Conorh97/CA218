4304343941fdba294c9be21fa8729e8a9e16f641
Conor Hanlon <conor.hanlon5@mail.dcu.ie>
2016-11-14 20:16:05 +0000

    Update 5-2.sql

--- week5/5-2.sql
+++ week5/5-2.sql
@@ -1,3 +1,7 @@
+-- Return the most common weekday which people borrow films on. Also
+-- print out the total number of films borrowed on that weekday.
+
+
 SELECT DAYNAME(rental_date) AS Day, COUNT(rental_id) as myCount
 FROM rental 
 GROUP BY Day
