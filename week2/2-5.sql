9e49cb19be966f6cd8af8b8b3cf694a673e2e763
Conor Hanlon <conor.hanlon5@mail.dcu.ie>
2016-11-14 20:05:46 +0000

    Update 2-5.sql

--- week2/2-5.sql
+++ week2/2-5.sql
@@ -1,3 +1,6 @@
+-- List all the information for customer that is either from the USA or Ireland, order the
+-- results by customerName alphabetical order.
+
 SELECT * 
 FROM customers 
 WHERE country = "USA" OR "Ireland" 
