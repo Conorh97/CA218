ddee47d74e9400d36c5483d00597f82a0069a9b0
Conor Hanlon <conor.hanlon5@mail.dcu.ie>
2016-11-14 20:18:57 +0000

    Update 6-2.sql

--- week6/6-2.sql
+++ week6/6-2.sql
@@ -1,3 +1,6 @@
+-- List the name of the city and the average time in days it takes
+-- between an order being placed and an order being shipped.
+
 SELECT c.city, AVG(DATEDIFF(o.shippedDate, o.orderDate)) AS Average 
 FROM customers c, orders o
 WHERE c.customerNumber = o.customerNumber
