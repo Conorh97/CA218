82692be156ca88f50bd36889f0ca1bab38067d72
Conor Hanlon <conor.hanlon5@mail.dcu.ie>
2016-11-14 20:06:25 +0000

    Update 2-6.sql

--- week2/2-6.sql
+++ week2/2-6.sql
@@ -1,3 +1,6 @@
+-- Return the products in products table, where their buyPrice is greater than 40, and have
+-- more than 300 quantity in stock, order the results by product name in alphabetical order.
+
 SELECT * 
 FROM products 
 WHERE buyPrice > 40 AND quantityInStock > 300 
