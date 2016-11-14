05c5aed9dae5918ee0721086766ea702a5cd646e
Conor Hanlon <conor.hanlon5@mail.dcu.ie>
2016-11-14 20:07:48 +0000

    Update 3-2.sql

--- week3/3-2.sql
+++ week3/3-2.sql
@@ -1,3 +1,7 @@
+-- For each order.
+-- Print the orderDate, the status and the total price of
+-- the order. Order your results from highest price to lowest.
+
 SELECT orderDate, status, amount 
 FROM orders o, payments p 
 WHERE o.customerNumber = p.customerNumber 
