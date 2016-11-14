6fa2fe3d64c4b4599fac891f0fec273e59017f88
Conor Hanlon <conor.hanlon5@mail.dcu.ie>
2016-11-14 20:15:30 +0000

    Update 5-1.sql

--- week5/5-1.sql
+++ week5/5-1.sql
@@ -1,3 +1,6 @@
+-- List the id and the title of film that been borrowed more than once by
+-- customers whose first names begin with ‘D’.
+
 SELECT f.film_id, f.title 
 FROM film f, inventory i, rental r
 WHERE f.film_id = i.film_id and (SELECT COUNT(r.inventory_id)
